634e31e0eec39499c7096ddc6573eb1dc648219e
Rara212 <78282415+Rara212@users.noreply.github.com>
2022-12-02 10:53:07 +0700

    added homescreen intents

--- app/src/main/java/com/example/asr/WelcomeActivity.kt
+++ app/src/main/java/com/example/asr/WelcomeActivity.kt
@@ -1,11 +1,32 @@
 package com.example.asr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var btnSignIn : Button
+    lateinit var btnNewAccount: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnSignIn = findViewById(R.id.btnSignIn)
+        btnNewAccount = findViewById(R.id.btnNewAccount)
+
+        /*Moving to main activity*/
+        btnSignIn.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+        /*Moving to creat account*/
+        btnNewAccount.setOnClickListener {
+            Intent(this, CreateAccount::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
