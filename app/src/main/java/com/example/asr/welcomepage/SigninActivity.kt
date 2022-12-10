4f3904f4ab3408044a385f8bbd3cf7ef3f2b90f5
shafanhd <shabilacollege@gmail.com>
2022-12-10 17:32:42 +0700

    edit signin

--- app/src/main/java/com/example/asr/welcomepage/SigninActivity.kt
+++ app/src/main/java/com/example/asr/welcomepage/SigninActivity.kt
@@ -5,16 +5,27 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.asr.homepage.MainActivity
 import com.example.asr.R
+import com.example.asr.api.RetrofitHelper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class SigninActivity : AppCompatActivity() {
 
-    lateinit var btnLogIn : Button
-    lateinit var etUsername : EditText
+    lateinit var btnLogIn: Button
+    lateinit var Email: EditText
+
+    val apiKey = ""
+    val token = "Bearer $apiKey"
+
+    val todoApi = RetrofitHelper.getInstance() create (UserApi::class.java)
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +33,10 @@ class SigninActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signin)
 
         btnLogIn = findViewById(R.id.btnLogIn)
-        etUsername = findViewById(R.id.etUsername)
+        Email = findViewById(R.id.etUsername)
         btnLogIn.setOnClickListener {
 
-            if (etUsername.text.isEmpty()) {
+            if (Email.text.isEmpty()) {
                 Toast.makeText(
                     applicationContext,
                     "Harap isi nama terlebih dahulu",
@@ -34,19 +45,54 @@ class SigninActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            var username = etUsername.text.toString()
+            var Email = Email.text.toString()
 
-            val sharedPreference =  getSharedPreferences(
+            val sharedPreference = getSharedPreferences(
                 "app_preference", Context.MODE_PRIVATE
             )
 
             var editor = sharedPreference.edit()
-            editor.putString("username", username)
+            editor.putString("email", Email)
             editor.commit()
 
             val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("result", username)
+            intent.putExtra("result", Email)
             startActivity(intent)
+        }
+        private fun signIn(email: String, password: String) {
+            CoroutineScope(Dispatchers.IO).launch {
+
+                val data = Users(email = email, password = password)
+                val response = todoApi.signIn(token = token, apiKey = apiKey, data = data)
+
+                val bodyResponse = if (response.code() == 200) {
+                    response.body()?.string()
+                } else {
+                    response.errorBody()?.string().toString()
+                }
+
+                var failed = false
+                val jsonResponse = JSONObject(bodyResponse)
+                if (jsonResponse.keys().asSequence().toList().contains("error")) {
+                    failed = true
+                }
+
+                var msg = ""
+                if (!failed) {
+                    var email = jsonResponse.getJSONObject("user").get("email")
+                    msg = "Successfully login! Welcome back: $email"
+                } else {
+                    msg += jsonResponse.get("error_description").toString()
+                }
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    Toast.makeText(
+                        applicationContext,
+                        msg,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
+}
