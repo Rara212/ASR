c15abcfb894b9db703cd2bde7b0b0c9bf08359a9
Rara212 <78282415+Rara212@users.noreply.github.com>
2022-12-15 13:07:03 +0700

    changed background

--- app/src/main/java/com/example/asr/settings/AboutUs.kt
+++ app/src/main/java/com/example/asr/settings/AboutUs.kt
@@ -1,9 +1,11 @@
 package com.example.asr.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.asr.R
+import kotlinx.android.synthetic.main.activity_about_us.*
 
 class AboutUs : AppCompatActivity() {
 
@@ -12,6 +14,10 @@ class AboutUs : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
 
-
+        btnbackAboutUs.setOnClickListener {
+            Intent(this, SettingActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
