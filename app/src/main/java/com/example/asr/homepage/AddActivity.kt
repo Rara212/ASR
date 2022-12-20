aa8750ac95fa1b680f90117902f712dc4994aeb9
shafanhd <shabilacollege@gmail.com>
2022-12-20 23:17:34 +0700

    added toast guide on radio button activity

--- app/src/main/java/com/example/asr/homepage/AddActivity.kt
+++ app/src/main/java/com/example/asr/homepage/AddActivity.kt
@@ -18,6 +18,12 @@ class AddActivity : AppCompatActivity() {
 
     lateinit var etActivity : EditText
     lateinit var btnAdd : Button
+    lateinit var rgAddActivity : RadioGroup
+    lateinit var rbUI: RadioButton
+    lateinit var rbNUI: RadioButton
+    lateinit var rbUNI: RadioButton
+    lateinit var rbNUNI: RadioButton
+
 
     val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maWZ5d2JnY3FrZmFzbmhjaWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NTUxMzYsImV4cCI6MTk4NTUzMTEzNn0.EqjggAQEzg4acUUzrwVxncdxNOiGP3VYO9Wd2yRz_LA"
     val token = "Bearer $apiKey"
@@ -30,6 +36,25 @@ class AddActivity : AppCompatActivity() {
 
         btnAdd = findViewById(R.id.btnAdd)
         etActivity = findViewById(R.id.etActivity)
+        rgAddActivity = findViewById(R.id.rgType)
+        rbUI = findViewById(R.id.rb1)
+        rbUNI = findViewById(R.id.rb2)
+        rbNUI = findViewById(R.id.rb3)
+        rbNUNI = findViewById(R.id.rb4)
+
+        rbUI.setOnClickListener {
+            Toast.makeText(this, "This is a Quadrant of Urgency", Toast.LENGTH_SHORT).show()
+        }
+        rbUNI.setOnClickListener {
+            Toast.makeText(this, "This is a Quadrant of Ditraction", Toast.LENGTH_SHORT).show()
+        }
+        rbNUI.setOnClickListener {
+            Toast.makeText(this, "This is a Quadrant of Quality", Toast.LENGTH_SHORT).show()
+        }
+        rbNUNI.setOnClickListener {
+            Toast.makeText(this, "This is a Quadrant of Waste", Toast.LENGTH_SHORT).show()
+        }
+
 
         val sharedPreference = getSharedPreferences(
             "app_preference", Context.MODE_PRIVATE
@@ -49,6 +74,8 @@ class AddActivity : AppCompatActivity() {
                     "New activity successfully added",
                     Toast.LENGTH_SHORT
                 ).show()
+
+
 
                 finish()
             }
