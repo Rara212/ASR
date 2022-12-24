f2b9dee82f2ff86f0d5fe35fb5ad34af89f06df2
Rara212 <78282415+Rara212@users.noreply.github.com>
2022-12-25 00:01:34 +0700

    added read radio button function activity

--- app/src/main/java/com/example/asr/homepage/settings/UpdateActivity.kt
+++ app/src/main/java/com/example/asr/homepage/settings/UpdateActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Audio.Radio
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
@@ -21,8 +22,13 @@ class UpdateActivity : AppCompatActivity() {
 
     lateinit var etActivityUpdate: EditText
     lateinit var btnUpdate: Button
+    lateinit var type1: RadioButton
+    lateinit var type2: RadioButton
+    lateinit var type3: RadioButton
+    lateinit var type4: RadioButton
 
     lateinit var id : String
+    lateinit var category : String
     val apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maWZ5d2JnY3FrZmFzbmhjaWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk5NTUxMzYsImV4cCI6MTk4NTUzMTEzNn0.EqjggAQEzg4acUUzrwVxncdxNOiGP3VYO9Wd2yRz_LA"
     val token = "Bearer $apiKey"
 
@@ -35,22 +41,33 @@ class UpdateActivity : AppCompatActivity() {
 
         etActivityUpdate = findViewById(R.id.etActivityUpdate)
         btnUpdate = findViewById(R.id.btnAddupdate)
+        type1 = findViewById(R.id.rb1)
+        type2 = findViewById(R.id.rb2)
+        type3 = findViewById(R.id.rb3)
+        type4 = findViewById(R.id.rb4)
 
+        val checkedActivityRadioButtonId = rgType.checkedRadioButtonId
+        val type= findViewById<RadioButton>(checkedActivityRadioButtonId)
+
+        category = intent.getStringExtra("category").toString()
         id = intent.getStringExtra("activityid").toString()
         var activityidQuery = "eq.$id"
 
+        /*Showing existing data*/
         etActivityUpdate.setText(intent.getStringExtra("activity").toString())
 
-        val sharedPreference = getSharedPreferences(
-            "app_preference", Context.MODE_PRIVATE
-        )
-
-        var userid = sharedPreference.getString("userid", "[No userid found]")
+        if(category==type1.text.toString()) {
+            type1.setChecked(true)
+        } else if (category==type2.text.toString()) {
+            type1.setChecked(true)
+        } else if (category==type3.text.toString()) {
+            type3.setChecked(true)
+        } else {
+            type4.setChecked(true)
+        }
 
         btnUpdate.setOnClickListener{
             CoroutineScope(Dispatchers.Main).launch {
-                val checkedActivityRadioButtonId = rgType.checkedRadioButtonId
-                val type= findViewById<RadioButton>(checkedActivityRadioButtonId)
                 val data = ActivityData(activity = etActivityUpdate.text.toString(), category = type.text.toString())
                 val response = activityApi.update(token = token, apiKey = apiKey, idQuery = activityidQuery, activityData = data)
 
